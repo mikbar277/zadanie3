a52a9b470f5cb633091950e6b93f14f7cb186439
mikbar277 <s25348@pjwstk.edu.pl>
2024-05-12 10:58:03 +0200

    First commit

---
+++ WebApplicationHorizontal/Controllers/AnimalsController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Model;
+using WebApplication1.Services;
+
+namespace WebApplication1.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AnimalsController : ControllerBase
+{
+    private IAnimalsService _animalsService;
+
+    public AnimalsController(IAnimalsService animalsService)
+    {
+        _animalsService = animalsService;
+    }
+    
+    // Endpoint used to return list of animals.
+    [HttpGet]
+    public IActionResult GetAnimals(string orderBy = "")
+    {
+        var animals = _animalsService.GetAnimals(orderBy);
+        return Ok(animals);
+    }
+
+    [HttpPost]
+    public IActionResult CreateAnimal(Animal animal)
+    {
+        var affectedCount = _animalsService.CreateAnimal(animal);
+        return StatusCode(StatusCodes.Status201Created);
+    }
+
+    [HttpPut("{idAnimal:int}")]
+    public IActionResult UpdateAnimal(int idAnimal, Animal animal)
+    {
+        var affectedCount = _animalsService.UpdateAnimal(idAnimal, animal);
+        return NoContent();
+    }
+
+    [HttpDelete("{idAnimal:int}")]
+    public IActionResult DeleteAnimal(int idAnimal)
+    {
+        var affectedCount = _animalsService.DeleteAnimal(idAnimal);
+        return NoContent();
+    }
+}
---
+++ WebApplicationHorizontal/Repositories/AnimalsRepository.cs
@@ -0,0 +1,187 @@
+﻿using System.Data.SqlClient;
+using System.Security.Cryptography;
+using WebApplication1.Model;
+
+namespace WebApplication1.Repositories;
+
+public class AnimalsRepository : IAnimalsRepository
+{
+    private IConfiguration _configuration;
+
+    public AnimalsRepository(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public IEnumerable<Animal> GetAnimals(string orderBy)
+    {
+        using var connection = new SqlConnection(_configuration["ConnectionStrings:DefaultConnection"]);
+        connection.Open();
+
+        if (orderBy == "name")
+        {
+            using var cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandText = "SELECT Animal.IdAnimal, Animal.Name, Animal.Description, Animal.Category, Animal.Area FROM Animal ORDER BY CASE @OrderBy WHEN 'name' THEN Name END ASC";
+            cmd.Parameters.AddWithValue("@OrderBy", "name");
+            
+            var dr = cmd.ExecuteReader();
+            var animals = new List<Animal>();
+            while (dr.Read())
+            {
+                var grade = new Animal
+                {
+                    IdAnimal = (int)dr["IdAnimal"],
+                    Name = dr["Name"].ToString(),
+                    Area = dr["Area"].ToString(),
+                    Category = dr["Category"].ToString(),
+                    Description = dr["Description"].ToString()
+                };
+                animals.Add(grade);
+            }
+            return animals;
+        }
+        else if (orderBy == "description")
+        {
+            using var cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandText = "SELECT Animal.IdAnimal, Animal.Name, Animal.Description, Animal.Category, Animal.Area FROM Animal ORDER BY CASE @OrderBy WHEN 'description' THEN Description END ASC";
+            cmd.Parameters.AddWithValue("@OrderBy", "description");
+            
+            var dr = cmd.ExecuteReader();
+            var animals = new List<Animal>();
+            while (dr.Read())
+            {
+                var grade = new Animal
+                {
+                    IdAnimal = (int)dr["IdAnimal"],
+                    Name = dr["Name"].ToString(),
+                    Area = dr["Area"].ToString(),
+                    Category = dr["Category"].ToString(),
+                    Description = dr["Description"].ToString()
+                };
+                animals.Add(grade);
+            }
+            return animals;
+        }
+        else if (orderBy == "category")
+        {
+            using var cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandText = "SELECT Animal.IdAnimal, Animal.Name, Animal.Description, Animal.Category, Animal.Area FROM Animal ORDER BY CASE @OrderBy WHEN 'category' THEN Category END ASC";
+            cmd.Parameters.AddWithValue("@OrderBy", "category");
+            
+            var dr = cmd.ExecuteReader();
+            var animals = new List<Animal>();
+            while (dr.Read())
+            {
+                var grade = new Animal
+                {
+                    IdAnimal = (int)dr["IdAnimal"],
+                    Name = dr["Name"].ToString(),
+                    Area = dr["Area"].ToString(),
+                    Category = dr["Category"].ToString(),
+                    Description = dr["Description"].ToString()
+                };
+                animals.Add(grade);
+            }
+            return animals;
+        }
+        else if (orderBy == "area")
+        {
+            using var cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandText = "SELECT Animal.IdAnimal, Animal.Name, Animal.Description, Animal.Category, Animal.Area FROM Animal ORDER BY CASE @OrderBy WHEN 'area' THEN Area END ASC";
+            cmd.Parameters.AddWithValue("@OrderBy", "area");
+            
+            var dr = cmd.ExecuteReader();
+            var animals = new List<Animal>();
+            while (dr.Read())
+            {
+                var grade = new Animal
+                {
+                    IdAnimal = (int)dr["IdAnimal"],
+                    Name = dr["Name"].ToString(),
+                    Area = dr["Area"].ToString(),
+                    Category = dr["Category"].ToString(),
+                    Description = dr["Description"].ToString()
+                };
+                animals.Add(grade);
+            }
+            return animals;
+        }
+        else
+        {
+            using var cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandText = "SELECT Animal.IdAnimal, Animal.Name, Animal.Description, Animal.Category, Animal.Area FROM Animal ORDER BY Name";
+
+            var dr = cmd.ExecuteReader();
+            var animals = new List<Animal>();
+            while (dr.Read())
+            {
+                var grade = new Animal
+                {
+                    IdAnimal = (int)dr["IdAnimal"],
+                    Name = dr["Name"].ToString(),
+                    Area = dr["Area"].ToString(),
+                    Category = dr["Category"].ToString(),
+                    Description = dr["Description"].ToString()
+                };
+                animals.Add(grade);
+            }
+
+            return animals;
+        }
+    }
+
+    public int CreateAnimal(Animal animal)
+    {
+        using var con = new SqlConnection(_configuration["ConnectionStrings:DefaultConnection"]);
+        con.Open();
+
+        using var cmd = new SqlCommand();
+        cmd.Connection = con;
+        cmd.CommandText = "INSERT INTO Animal(IdAnimal, Name, Description, Category, Area) VALUES(@IdAnimal, @Name, @Description, @Category, @Area)";
+        cmd.Parameters.AddWithValue("@IdAnimal", animal.IdAnimal);
+        cmd.Parameters.AddWithValue("@Name", animal.Name);
+        cmd.Parameters.AddWithValue("@Description", animal.Description);
+        cmd.Parameters.AddWithValue("@Category", animal.Category);
+        cmd.Parameters.AddWithValue("@Area", animal.Area);
+
+        var affectedCount = cmd.ExecuteNonQuery();
+        return affectedCount;
+    }
+
+    public int UpdateAnimal(int id, Animal animal)
+    {
+        using var connection = new SqlConnection(_configuration["ConnectionStrings:DefaultConnection"]);
+        connection.Open();
+
+        using var cmd = new SqlCommand();
+        cmd.Connection = connection;
+        cmd.CommandText = "UPDATE Animal SET Name=@Name, Description=@Description, Category=@Category, Area=@Area WHERE IdAnimal = @IdAnimal";
+        cmd.Parameters.AddWithValue("@IdAnimal", id);
+        cmd.Parameters.AddWithValue("@Name", animal.Name);
+        cmd.Parameters.AddWithValue("@Description", animal.Description);
+        cmd.Parameters.AddWithValue("@Category", animal.Category);
+        cmd.Parameters.AddWithValue("@Area", animal.Area);
+
+        var affectedCount = cmd.ExecuteNonQuery();
+        return affectedCount;
+    }
+
+    public int DeleteAnimal(int id)
+    {
+        using var con = new SqlConnection(_configuration["ConnectionStrings:DefaultConnection"]);
+        con.Open();
+
+        using var cmd = new SqlCommand();
+        cmd.Connection = con;
+        cmd.CommandText = "DELETE FROM Animal WHERE IdAnimal = @IdAnimal";
+        cmd.Parameters.AddWithValue("@IdAnimal", id);
+
+        var affectedCount = cmd.ExecuteNonQuery();
+        return affectedCount;
+    }
+}
